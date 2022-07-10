85421f5433a761da8a4859dc0a909116c5a335ef
Ofido <danieldsb98@gmail.com>
2022-07-10 13:07:54 -0300

    aula4.1

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,15 @@
 import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
+import { takeUntil } from './utils/operators.js';
 import { log } from './utils/promise-helpers.js';
 
-document
-.querySelector('#myButton')
-.onclick = () =>
+const operation = takeUntil(3, () =>
     service
     .sumItens('2143')
     .then(log)
     .catch(console.error)
+)
+
+document
+.querySelector('#myButton')
+.onclick = operation;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -11,3 +11,6 @@ export const compose = (...fns) => value =>
 export const pipe = (...fns) => value =>
     fns.reduce((previousValue, fn) =>
         fn(previousValue), value)
+
+export const takeUntil = (times, fn) =>
+    () => times-- > 0 && fn()
